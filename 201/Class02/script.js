5f5bb1c23fef9744a29a4332818ecfdee6e2aadb
Peanut <123972373+prenticem@users.noreply.github.com>
2023-03-23 13:51:36 -0500

    update

---
+++ 201/Class02/script.js
@@ -0,0 +1,43 @@
+let answer1= prompt("Do yo need coffee?").toLowerCase()
+if (answer1 == 'y' || answer1 == 'yes') {
+    alert("You Got It!")
+} else if (answer1 == 'n' || answer1 == 'no'){
+    alert("Try Again")
+} else {
+    ("HaHa No No")
+}
+let answer2=  prompt("Do you have silblings?").toLowerCase()
+if (answer2 == 'y' || answer2 == 'yes') {
+    alert("Cool Beans You Got It")
+} else if (answer2 == 'n' || answer2 'no'){
+    alert("Try Again")
+} else {
+    ("HaHa No No")
+}
+let answer3= prompt("Do you like pizza?").toLowerCase()
+if (answer3 == 'y' || answer3 == 'yes') { 
+    alert("Let's Go")
+} else if (answer3 == 'n' || answer3 == 'no'){
+    alert("No Way")
+} else{
+    ("Almost Had It")
+
+}
+let answer4= prompt("Do you drive").toLowerCase()
+if (answer4 == 'y' || answer4 == 'yes') {
+    alert("Your Great")
+} else if (answer4 == 'n' || answer4 == 'no'){
+    alert("You Are Not Close")
+} else {
+    ("You Are Not The Goat")
+}
+let answer5= prompt("Are you a US cittizen?").toLowerCase()
+if (answer5 == 'y' || answer5 == 'yes') {
+    alert("Your Alsome 365")
+} else if (answer5 == 'n' || answer5 == 'no'){
+
+}else {
+    ("You Got To Get Closer Than That")
+}
+let prompt=("What Is Your Name?");
+alert("what Is Your Name?" == " is a good sports team!");
